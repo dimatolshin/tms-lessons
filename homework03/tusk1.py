84b060b7fe20488b0926db38d9c183340a6f2d12
dima_toslhin <dima_toslhin@mail.ru>
2023-01-29 20:34:52 +0300

    homework1

---
+++ homework03/tusk1.py
@@ -0,0 +1,7 @@
+a=int(input())
+opration1=a**2
+operation2=a*4
+operation3=((a+a)**2)**0.5
+print("Площаль квадрата-",opration1,)
+print("Препиметр квадрата-",operation2,)
+print("Диагональ квадрата-",int(operation3))
