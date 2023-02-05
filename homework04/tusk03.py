e79a1411d19446d9e4fdd4a2d2365392007e610a
dima_toslhin <dima_toslhin@mail.ru>
2023-02-06 01:12:25 +0300

    homework04

---
+++ homework04/tusk03.py
@@ -0,0 +1,18 @@
+for i in range(0,101):
+    print(i)
+    while True:
+        a = input("Should we break?")
+        if a == "yes":
+            break
+        if a == "no":
+            break
+        if a != "yes" and a!="no":
+            print("Don't understand you")
+    if a == "yes":
+        break
+
+
+
+
+
+
