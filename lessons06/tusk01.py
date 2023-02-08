10b94f0a450f9a0d32b395ee45acc293401b9a61
dima_toslhin <dima_toslhin@mail.ru>
2023-02-08 18:14:15 +0300

    classroom

---
+++ lessons06/tusk01.py
@@ -0,0 +1,79 @@
+#1) a=int(input())
+#b=int(input())
+#if a==b:
+#    print(True)
+#else:
+#    print(False)
+
+#2)a=int(input())
+#b=int(input())
+#if a>b:
+#    print(a)
+#else:
+#    print(b)
+
+#3)a=int(input())
+#if a>0:
+#    print("Yes")
+#else:
+#    print("No")
+
+#4)a=int(input())
+#if a>0:
+#    print("positive")
+#if a<0:
+#    print("negative")
+#if a==0:
+#    print("zero")
+
+#5)a=int(input())
+#c=int(input())
+#b=int(input())
+#if a>0 and c>0 and b>0:
+#    print("yes")
+#else:
+#    print("no")
+
+#6)a=int(input())
+#if a==12 or 0<a<3:
+#    print("zima")
+#if 2<a<6:
+#    print("vesna")
+#if 5<a<9:
+#    print("leto")
+#if 8<a<12:
+#    print("osen")
+
+#7)a=int(input())
+#b=int(input())
+#c=int(input())
+#k=[]
+#if a>0:
+#    k.append(a)
+#if b>0:
+#    k.append(b)
+#if c>0:
+#    k.append(c)
+#else:
+#    print(len(k))
+#    exit()
+#print(len(k))
+
+#8) a=int(input())
+# c=int(input())
+# b=int(input())
+# if c<a>b:
+#     print(a)
+# if a<c>b:
+#     print(c)
+# else:
+#     print(b)
+
+
+
+
+
+
+
+
+
