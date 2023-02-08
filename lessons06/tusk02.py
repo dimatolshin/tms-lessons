aba36b36fc5860c6a36829931fe1cf6b0579ef23
dima_toslhin <dima_toslhin@mail.ru>
2023-02-08 18:14:33 +0300

    classroom

---
+++ lessons06/tusk02.py
@@ -0,0 +1,63 @@
+#1) #sum=0
+# for i in range(1,11):
+#     sum+=i
+# print(sum)
+
+#2)a=list(input().split())
+#sum=0
+#for i in a:
+#    sum+=int(i)
+#print(sum)
+
+#3)a=max(list(map(int,input().split())))
+#print(a)
+
+#4) a=(list(map(int,input().split())))
+# b=False
+# for i in a:
+#     if i==0:
+#         b=True
+#         break
+# if b:
+#     print("yes")
+# else:
+#     print("no")
+
+#5) a=input()
+# b=[]
+# for i in a:
+#     if i=="a":
+#         b.append(i)
+# dlina=len(b)
+# print(int(dlina))
+
+#6)lol={"a":2,"b":3,"c":8,"d":7}
+#a=0
+#for m in lol.values():
+#    if int(m)>a:
+#        a=int(m)
+#print(a)
+
+#6.1)ict_example = {"a":2,"b":3,"c":8,"d":7}
+#a=(max(ict_example.values()))
+#print(a)
+
+#7) example = {"a": 1, "b": 2}
+# a=(max(example.items(), key=lambda x: x[1])[0])
+# print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
