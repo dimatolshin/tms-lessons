c8e91341e54a4c48446abe1215f24aaa211c43f9
dima_toslhin <dima_toslhin@mail.ru>
2023-03-18 16:12:27 +0300

    homework

--- homework04/task10.py
+++ homework04/task10.py
@@ -2,7 +2,7 @@ def get_most_frequent_word():
     user = list(map(str, input().split()))
     word = ''
     for i in user:
-        if user.count(i) > word.count(i):
+        if user.count(i) > user.count(word):
             word = i
     return word
 
