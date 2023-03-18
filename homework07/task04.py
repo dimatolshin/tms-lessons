ecd428a07bf92956c488615e3fa5d28e1b4484f0
dima_toslhin <dima_toslhin@mail.ru>
2023-03-18 16:20:34 +0300

    homework 7

--- homework07/task04.py
+++ homework07/task04.py
@@ -2,21 +2,10 @@
 #Вам нужно написать функцию my_join, которая принимает список из строк и символ-разделить,
 # и возвращает строку, в которой все слова из списка соединены через символ разделитель.
 
-#                                1 method
-# user=list(map(str,input().split()))
-# def my_join(user):
-#     s = "@"
-#     return s.join(user)
-# print(my_join(user))
 
-
-#                                 2 method
-
-# from functools import reduce
-# user = input().split()
-# s = input()
-# def my_join():
-#      return [(i + s) for i in user]
-# agard=reduce(lambda x,y: x+y , my_join())
-# agard=agard[:-1]
-# print(agard)
+from functools import reduce
+user = input().split()
+s = input()
+def my_join(user):
+    return reduce(lambda x,y: x+s+y , user)
+print(my_join(user))
